bbdf71933b877f9698dd0caffe4865164e15a5b0
Param Raghavan <paramraghavan@gmail.com>
2024-01-06 22:27:48 -0500

    add airport code as label to the nodes.

--- render_graph/render_graph.py
+++ render_graph/render_graph.py
@@ -27,9 +27,17 @@ print(results)
 # Create a NetworkX graph to represent the data
 G = nx.Graph()
 
+"""
+a = g.V(vertexId).next()
+a.label
+p = g.V('3').properties().toList()
+vm = g.V('3').valueMap().next()
+"""
+
 # Add airports as nodes
 for result in results:
-    airport_id = result['id']
+    # result ==> {'id': 9664, 'label': 'airport', 'properties': {'code': ['LHR'], 'name': ['Heathrow Airport']}}
+    airport_id = result['properties']['code'][0] #result['id']
     airport_name = result['properties']['name'][0]
     G.add_node(airport_id, name=airport_name, label=airport_name)
 
@@ -46,21 +54,35 @@ results = g.E().project('id', 'label', 'outV', 'inV', 'properties')\
      .toList()
 
 # Add routes as edges
+for result in results:results = g.E().project('id', 'label', 'outV', 'inV', 'properties')\
+     .by(T.id)\
+     .by(T.label)\
+     .by(__.outV().id())\
+     .by(__.inV().id())\
+     .by(__.valueMap())\
+     .toList()
+
+# Add routes as edges
 for result in results:
-    source_airport_id = result['outV']
-    destination_airport_id = result['inV']
-    G.add_edge(source_airport_id, destination_airport_id)
+    # result => {'id': 22016, 'label': 'route', 'outV': 3458, 'inV': 4739, 'properties': {'miles': 7459}}
+    source_vm = g.V(result['outV']).valueMap().next()
+    print(source_vm) # {'code': ['CDG'], 'name': ['Charles de Gaulle Airport']}
+    source_airport_id = source_vm['code']
+    destination_vm = g.V(result['inV']).valueMap().next()
+    print(destination_vm)
+    destination_airport_id = destination_vm['code']
+    G.add_edge(source_airport_id[0], destination_airport_id[0], miles=result['properties']['miles'])
 
 # Visualize the graph
 pos = nx.spring_layout(G)
 plt.figure(figsize=(10, 10))
 nx.draw(G, pos, with_labels=True, node_size=1000, node_color='skyblue', font_size=8)
-# Drawing labels on the nodes
-# The labels parameter should be a dictionary mapping node names to labels
-# If you want to use default labels (which are node names), you can do this:
-for item in G.nodes.data():
-    print(item[0], item[1]['name'])
-labels = {item[0]: item[1]['name'] for node in G.nodes.data()}
-nx.draw_networkx_labels(G, pos, labels=labels, font_size=8)
+# # Drawing labels on the nodes
+# # The labels parameter should be a dictionary mapping node names to labels
+# # If you want to use default labels (which are node names), you can do this:
+# for item in G.nodes.data():
+#     print(item[0], item[1]['name'])
+# labels = {item[0]: item[1]['name'] for node in G.nodes.data()}
+# nx.draw_networkx_labels(G, pos, labels=labels, font_size=8)
 plt.title('Airport Routes Graph')
 plt.show()
